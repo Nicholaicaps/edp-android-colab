707d67850f2cd017b0552c2b07cdcb5204aa3dc5
Nicholai Patrick Capinpuyan <ncapinpuyan46671@liceo.edu.ph>
2026-08-13 14:15:53 +0800

    Lab Activity 8: MVVM user profile app

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,12 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,27 +19,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingApp()
+                    ProfileScreen()
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun GreetingApp() {
-    val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Home) {
-        composable<Home> {
-            HomeScreen(onShowGreeting = { typedName ->
-                // Pass the name by creating a Greeting route object
-                navController.navigate(Greeting(userName = typedName))
-            })
-        }
-        composable<Greeting> { backStackEntry ->
-            // Rebuild the typed Greeting object on this screen
-            val greeting: Greeting = backStackEntry.toRoute()
-            GreetingScreen(userName = greeting.userName)
         }
     }
 }
